9b09fef2b795c3fcbcfc71dd895e36edb6015b05
EduardoCava221 <gladimircavalheirodasilva056@gmail.com>
2024-08-14 15:53:36 -0300

    Add files via upload

---
+++ aula 03/index.js
@@ -0,0 +1,17 @@
+/* | Celsius |   Fahrenheit   | Saida real |
+   | 12      |      53,6      |   53,6     |
+   | 280     |      536       |    536     |
+   | 57      |     134,6      |   134,6    |
+   | 73      |     163,4      |   163,4    |
+   | 32      |      89,6      |    89,6    |
+
+*/
+let celsius
+let fahrenheit
+ 
+celsius = prompt("qual é o grau celsius?") 
+console.log("o numero de celsius é", celsius)
+fahrenheit = (celsius *1.8) + 32
+console.log("Em fahrenheit fica "), fahrenheit 
+
+
