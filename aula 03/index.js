4ca24cea921c0daf8caae1b8a44fa26f21ab12f4
EduardoCava221 <gladimircavalheirodasilva056@gmail.com>
2024-08-14 15:59:05 -0300

    index.js

--- aula 03/index.js
+++ aula 03/index.js
@@ -1,11 +1,3 @@
-/* | Celsius |   Fahrenheit   | Saida real |
-   | 12      |      53,6      |   53,6     |
-   | 280     |      536       |    536     |
-   | 57      |     134,6      |   134,6    |
-   | 73      |     163,4      |   163,4    |
-   | 32      |      89,6      |    89,6    |
-
-*/
 let celsius
 let fahrenheit
  
@@ -14,4 +6,14 @@ console.log("o numero de celsius é", celsius)
 fahrenheit = (celsius *1.8) + 32
 console.log("Em fahrenheit fica "), fahrenheit 
 
+
+/* | Celsius |   Fahrenheit   | Saida real |
+   | 12      |      53,6      |   53,6     |
+   | 280     |      536       |    536     |
+   | 57      |     134,6      |   134,6    |
+   | 73      |     163,4      |   163,4    |
+   | 32      |      89,6      |    89,6    |
+
+*/
+//sem erros
 
