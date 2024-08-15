629c80d811c7a14d22979c5bb35c6fa592c8a5b0
Gladimir Eduardo <gladimircavalheirodasilva056@gmail.com>
2024-08-15 15:16:32 -0300

    Add files via upload

---
+++ Aula 04/index.js
@@ -0,0 +1,31 @@
+/*1)
+let a = 10
+let b = 10
+
+definiu um número mudavel ou ajustavel para a letra a e b
+
+console.log(b);
+
+basicamente quando você chama console.log(b), o valor da variavel (b), será impresso no console do navegador.
+
+b=5
+mudou o valor da vaiavel b para 5
+console.log(a,b);
+imprimiu o valor da variavel de novo
+
+2)
+let a = 10
+let b = 20
+declarou duas variaveis usando o let que você pode alterar o valor 
+C = A  
+B = C  
+A = B 
+TENTOU ATRIBUIR VALORES C, B e A. NO ENTANTO, HA UM ERRO NA B e A 
+C FOI DEFINIDO COMO A, MAS A AINDA NAO FOI DECLARADO. PORTANTO ISSO RESULTARA EM ERRO.
+
+console.log(A, B, C);EXIBIRÁ 10 20 10 POIS C NAO FOI DECLARADO
+*/
+
+
+
+
