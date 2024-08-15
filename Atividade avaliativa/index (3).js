620c655a9918e668d7b57935e3d5e66b71cf7532
Gladimir Eduardo <gladimircavalheirodasilva056@gmail.com>
2024-08-15 14:16:14 -0300

    Add files via upload

---
+++ Atividade avaliativa/index (3).js
@@ -0,0 +1,19 @@
+let celsius
+let fahrenheit
+ 
+celsius = prompt("qual é o grau celsius?") 
+console.log("o numero de celsius é", celsius)
+fahrenheit = (celsius *1.8) + 32
+console.log("Em fahrenheit fica "), fahrenheit 
+
+
+/* | Celsius |   Fahrenheit   | Saida real |
+   | 12      |      53,6      |   53,6     |
+   | 280     |      536       |    536     |
+   | 57      |     134,6      |   134,6    |
+   | 73      |     163,4      |   163,4    |
+   | 32      |      89,6      |    89,6    |
+
+*/
+//sem erros
+
