cd7043541cf2529003a7292b537891b66e0547d4
Gladimir Eduardo <gladimircavalheirodasilva056@gmail.com>
2024-08-19 17:05:39 -0300

    Add files via upload

---
+++ Aula 06/index.js
@@ -0,0 +1,158 @@
+//aula 6 operadores aritmeticos
+
+/*const primeiroValor = 10
+const segundoValor=20
+const resultado = primeiroValor + segundoValor +  4
+
+console.log("o resultado é:", resultado);
+*/
+
+/*const primeiroValor = 10
+const segundoValor = 20
+
+const resultado = primeiroValor - segundoValor 
+console.log("o resultado é:", resultado);
+*/
+
+/*const primeiroValor = 10
+const segundoValor = 25
+const resultado = primeiroValor * segundoValor
+
+console.log("o resultado é:", resultado);
+*/
+
+/*const primeiroValor = 345
+const segundoValor = 10
+const resultado = primeiroValor / segundoValor
+
+console.log("o resultado é:", resultado);
+*/
+/*const restoDivisão = 11%4
+console.log("o resto da divisão é:", restoDivisão);
+*/
+
+
+/*let minhaIdade = 26
+minhaIdade = minhaIdade + 1
+console.log(minhaIdade);
+*/
+
+//Exercicio 1
+
+// 1) 
+/*const primeiroValor = 3
+const segundoValor = 4
+
+const resultado = 3 + 4
+
+console.log("o resultado é:", resultado);
+*/
+
+// 2)
+/*const primeiroValor = 3
+const segundoValor = 5
+const resultado = primeiroValor * segundoValor / 5
+console.log("o resultado é:", resultado);
+*/
+
+// 3)
+/*const primeiroValor = 5
+const segundoValor = 4
+const resultado = primeiroValor - segundoValor * -1
+console.log("o resultado é:", resultado);
+*/
+// 4)
+/*const restoDivisão = 234%5
+console.log("o resto da divisão é:", restoDivisão);
+*/
+// exemplo comparadores
+ // comparador =
+//const condição = 1 === 1
+//console.log(condição); //true
+
+//const condição = 1 === "1"
+//console.log(condição); // false
+
+// 2) diferente !==
+
+//const condição = 1 !== "1"
+//console.log(condição); //true
+
+//const condição = 2 !== 1 
+//console.log(condição); //true
+
+
+// 3) >, >=
+//const condição = 1 > 2
+//console.log (condição)   //false
+
+//const condição = 1 > "2"
+//console.log(condição); //false
+
+//const condição = 1 >= 1
+//console.log(condição); //true
+
+//const condição = 1 >= "2"
+//console.log(condição); //false
+
+//4) <, <=
+
+//const condição = 1 < 1
+//console.log(condição); //false
+
+//const condição = 1 < "2"
+//console.log(condição); //true
+
+//const condição = 1 <= 1
+//console.log(condição); //true
+
+//const condição = 1 <= "2" 
+//console.log(condição); //true
+
+//exercicio 2 
+
+const primeiroNúmero = 5
+const segundoNúmero = 8
+
+const condição = primeiroNúmero === segundoNúmero
+console.log("o primeiro número é igual o segundo?", condição);//false
+
+const condição2 = primeiroNúmero !== segundoNúmero
+console.log("o primeiro número é diferente do segundo?", condição2);//true
+
+const condição3 = primeiroNúmero > segundoNúmero
+console.log("o primeiro número é maior que o segundo?", condição3);//false
+
+const condição4 = primeiroNúmero < segundoNúmero
+console.log("o primeiro número é menor que o segundo?", condição4);//true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
