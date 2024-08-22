10f24d0006489b81a2b986f96771025530315cfb
Gladimir Eduardo <gladimircavalheirodasilva056@gmail.com>
2024-08-22 14:43:44 -0300

    Add files via upload

---
+++ Aula 07/index.js
@@ -0,0 +1,97 @@
+/* const a = true
+const b = true
+
+const resultado = 
+a && b
+console.log("o resultado é", resultado); //true
+
+const b2 = false
+const c = true
+const resultado2 = 
+b2 && c
+console.log("o resultado é", resultado2); //false
+
+const a2 = true
+const c2 =  true
+const resultado3 =
+a2 && c2
+console.log("o resultado é", resultado3); //true
+
+const a3 = true
+const b3 = false
+const c3 = true
+const resultado4 = 
+a3 && b3 && c3
+console.log("o resultado é", resultado4); //false
+*/
+
+/*const a = true
+const b = false
+const resultado = 
+a || b
+console.log("o resultado é", resultado); //true
+
+const b2 = false
+const c = true
+const resultado2 = 
+b2 || c
+console.log("o resultado é", resultado); //true
+
+const a2 = true
+const c2 = true
+const resultado3 = 
+a2 || c2
+console.log("o resultado é", resultado); //true
+
+const a3 = true
+const b3 = false
+const c3 = true
+const resultado4 = 
+a3 || b3 || c3
+console.log("o resultado é", resultado); //true
+*/
+
+/*const primeiroValor = false
+const resultado = !primeiroValor
+console.log(resultado); //false
+
+const primeiroValor2 = true
+const resultado2 = !primeiroValor2
+console.log(resultado2); //true
+*/
+
+/*const nome = prompt ("qual seu nome?")
+console.log("nome", nome);
+const anoNasci = prompt ("qual o ano que você nasceu?")
+const anoAtual = prompt ("qual o ano atual?")
+const idade = anoAtual - anoNasci
+console.log ("idade?", idade)
+const maiorIdade = idade >=18
+console.log("é maior de idade?", maiorIdade);
+const diferençadeAnos = 2050 - anoAtual
+console.log ("idade em 2050...", idade+diferençadeAnos)
+*/
+/*const bool1 = true  //gaveta 1 é true
+const bool2 = false //gaveta 2 é false
+const bool3 = !bool2 //gaveta 3 é true pq o sinal de "!" significa ser ao contrario do valor inserido que no caso é o valor de bool2, false.
+let resultado = bool1 && bool2 //true e false
+console.log("a.",resultado) //a. false pois true com false da false
+
+resultado = bool1 && bool2 && bool3 //true, false e true
+console.log ("b. ", resultado) //resultado, false pois é 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
