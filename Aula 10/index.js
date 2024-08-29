eb189b0fd80d3ce384c453c6a3fdf5f69e536f1c
Gladimir Eduardo <gladimircavalheirodasilva056@gmail.com>
2024-08-29 13:40:57 -0300

    Update index.js

--- Aula 10/index.js
+++ Aula 10/index.js
@@ -14,16 +14,10 @@ if(condição2){
 console.log("exibiu condição2");*/
 
 /* Exercicio 1
-const num1 = Number(prompt("digite um numero:"))
-const num2 = Number(prompt("digite outro numero:"))
-if (num1 === num2){
-    console.log("os numeros são iguais");
-    
-}
-if(num1!==num2){
-    console.log("os numeros sao diferentes");
-    
-}*/
+
+
+
+*/
 
 /* Exemplo 2 
 let condição = false
@@ -147,18 +141,72 @@ if (condiçao1 ){
     
 }*/
 
-/* Exercicio Faculdade
-let ensinoMedio = prompt("Você concluiu o ensino medio? (sim/não)")
-let idade = prompt("Voce tem mais de 18 anos? (sim/nao)")
-let cursandoOutraFaculdade = prompt ("Você esta cursando outra faculdade (sim/não)")
+/*let ensMedio = prompt('Você terminou o Ensino Médio?')
+if(ensMedio.toLowerCase() === 'sim') {
+  ensMedio = true
+}
 
-if (ensinoMedio.toLowerCase() === "sim" && idade > 18 &&
-cursandoOutraFaculdade.toLowerCase() === "não") {
-    console.log("voce pode estudar nessa faculdade.");
-    
-}else{
-    console.log("voce nao pode estudar nessa faculdade");
-    
-}*/
+let deMaior = prompt('Você tem mais de 18 anos?')
+if(deMaior.toLowerCase() === 'sim') {
+deMaior = true
+}
+
+let outraFac = prompt('Você está cursando outra faculdade?') 
+if(outraFac.toLowerCase() === 'não') {
+  outraFac = true
+} else {
+  outraFac = false
+}
+
+if(ensMedio&&deMaior&&outraFac) {
+console.log('Você pode cursar na nossa faculdade')
+}
+*/
+
+/* desafio
+
+let choose = Math.floor(Math.random()*3)
+// console.log(choose)
+
+let jokenpo = prompt('Escolha entre Pedra, Papel ou Tesoura')
+console.log(Você escolheu ${jokenpo})
+
+switch (choose) {
+  case 0:
+    console.log('Seu inimigo escolheu Tesoura')
+    break
+
+  case 1: 
+    console.log('Seu inimigo escolheu Papel')
+    break
+
+  case 2:
+    console.log('Seu inimigo escolheu Pedra')
+}
+
+if(jokenpo.toLowerCase() === 'pedra' && choose===0) {
+  console.log('Pedra amassa Tesoura, você ganhou!!')
+} else if(jokenpo.toLowerCase() === 'pedra' && choose===1) {
+  console.log('Papel enrola Pedra, você perdeu!!')
+} else if(jokenpo.toLowerCase()=== 'pedra' && choose===2) {
+  console.log('Pedra empata com pedra, vocês empataram!!')
+}
+
+if(jokenpo.toLowerCase() === 'tesoura' && choose===0) {
+  console.log('Tesoura não corta tesoura, vocês empataram!!')
+} else if(jokenpo.toLowerCase() === 'tesoura' && choose===1) {
+  console.log('Tesoura corta papel, você ganhou!!')
+} else if(jokenpo.toLowerCase()=== 'tesoura' && choose===2) {
+  console.log('Pedra amassa tesoura, você perdeu!!')
+}
+
+if(jokenpo.toLowerCase() === 'papel' && choose===0) {
+  console.log('Tesoura corta papel, você perdeu!!')
+} else if(jokenpo.toLowerCase() === 'papel' && choose===1) {
+  console.log('Papel não enrola papel, vocês empataram!!')
+} else if(jokenpo.toLowerCase()=== 'papel' && choose===2) {
+  console.log('Papel enrola pedra, você ganhou!!')
+}*/   
+
 
 
