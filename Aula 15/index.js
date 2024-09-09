263f81d8c4beb2052d161dff91d90236dd81c22b
Gladimir Eduardo <gladimircavalheirodasilva056@gmail.com>
2024-09-09 17:09:49 -0300

    Add files via upload

---
+++ Aula 15/index.js
@@ -0,0 +1,106 @@
+/*function calculaArea(largura,altura){
+    const area = largura * altura
+    console.log(area);
+    
+}
+calculaArea(2,3)
+calculaArea (5,4)
+calculaArea (7,5)
+
+function imprimirOlaMundo(){
+    console.log("Olá Mundo");
+    
+}
+imprimirOlaMundo()
+imprimirOlaMundo()*/
+
+
+/*function boasVindas(){
+    console.log("bem vindo turma 24-2t");
+    
+}
+boasVindas()*/
+
+/*function somarNumeros() {
+    const numero1 = 8;  
+    const numero2 = 12; 
+    const resultado = numero1 + numero2; 
+    console.log("A soma é:", resultado); 
+}
+
+somarNumeros(); */
+
+/*function dobrarNumero() {
+    const numero = 7;  
+    const resultado = numero * 2;  
+    console.log("O dobro de", numero, "é", resultado);  
+}
+
+dobrarNumero(); */
+
+/*function cumprimentar(nome) {
+    console.log(`Olá, ${nome}`);
+}
+cumprimentar("Ana");
+cumprimentar("Lucas");
+cumprimentar("Mariana");*/
+
+/*function somar(n1, n2) {
+    const soma = n1 + n2;
+    console.log(`A soma é ${soma}`);
+}
+
+somar(5, 10);
+somar(8, 15);
+somar(12, 20);*/
+
+/*function perguntarParaDiulie() {
+    const pergunta = "Diulie, qual é o seu hobby favorito?";
+    console.log(pergunta);
+}
+
+perguntarParaDiulie();*/
+
+ /*function calculaArea(largura, altura){
+    const resultadoArea = largura * altura
+    return resultadoArea
+ }
+
+ let area = calculaArea(2,5)
+console.log("a area é", area);*/
+
+/*function somar(a, b) {
+    return a + b;
+}
+
+let resultado = somar(5, 7);
+console.log("A soma é: " + resultado);*/
+
+/*function processarArray(arr) {
+    if (arr.length < 2) {
+        return []; 
+    }
+    
+    let primeiro = arr[0] / 2;
+    let ultimo = arr[arr.length - 1] / 2;
+    
+    return [primeiro, ultimo];
+}
+
+
+let arrayOriginal = [4, 8, 15, 16, 23, 42];
+let resultado = processarArray(arrayOriginal);
+console.log(resultado);*/ 
+
+/*const calculaArea = function(largura, altura){
+    const soma = largura * altura
+    return soma
+}
+const resultado = calculaArea(2,5)*/
+
+/*const soma =( n1, n2) => {
+    resultado = n1 + n2
+    console.log("a soma é:", resultado)
+}
+soma(76,98)*/
+
