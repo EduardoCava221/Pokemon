b9933ae1f8fa4f6d68ef0f75b75051f867efd728
Gladimir Eduardo <gladimircavalheirodasilva056@gmail.com>
2024-10-01 17:07:09 -0300

    Add files via upload

---
+++ Aula 22/index.js
@@ -0,0 +1,61 @@
+/*let numeros = [1,2,3,4,5]
+let soma = 0
+
+for (let i = 0; i < numeros.length; i++) {
+    soma += numeros[i];
+    
+}
+console.log(soma);*/
+
+/*function fatorial(n) {
+    let resultado = 1
+    for (let i = n; i < 0; i--) {
+        resultado *= i;
+        
+    }
+    return resultado
+}
+console.log(fatorial(5));
+
+/*|INTERAÇAO| i | Resultado |
+  |1        | 5 |     5     |
+  |2        | 4 |     20    |
+  |3        | 3 |    60     |
+  |4        | 2 |    120    |*/
+
+  /*let numeros = [1,2,3,4,5]
+  let soma = 0
+  for (let i = 0; i < numeros.length; i++){
+    console.log(`i: ${i},numeros[i]:${numeros[i]},soma antes: ${soma}`);
+    soma+=numeros[i]
+    console.log(`soma depois: ${soma}`);
+    
+  }
+  console.log(`resultado final:${soma}`);*/
+  
+  /*let numeros = [1,2,3,4,5]
+  let somaPares = 0
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] %2 === 0) {
+        somaPares += numeros[i]
+    }
+    
+  }
+  console.log(somaPares);*/
+ 
+  /*function jogoPontuaçao(jogadas) {
+    let pontuaçao = 0
+  }
+  for (let i = 0; i < jogadas.length; i++) {
+   if(jogadas[i]==="ganhou"){
+
+   }else if (jogadas[i]==="empatou"){
+   pontuaçao += 1
+   }else if (jogadas[i]==="perdeu"){
+ pontuaçao -= 1
+   }
+    return pontuaçao
+  } 
+
+  let resultadoFinal = jogoPontuaçao (["ganhou", "perdeu", "empatou", "ganhou"])
+console.log(`Pontuaçao final: ${resultadoFinal}`);*/
